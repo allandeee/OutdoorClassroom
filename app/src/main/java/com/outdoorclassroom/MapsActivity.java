0fa757e1a31e8a7a3df948933385219ac75ff46a
Allan Dominguez <allan.dominguez@students.mq.edu.au>
2017-04-27 11:57:56 +1000

    Added start and end markers for EHHW
    Eastern Hill Heritage Walk (EHHW)
    - added start point at East Esplanade
    - added end point at Sount Steyne

--- app/src/main/java/com/outdoorclassroom/MapsActivity.java
+++ app/src/main/java/com/outdoorclassroom/MapsActivity.java
@@ -7,12 +7,19 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    //2-dimensional array, containing array of walks
+    ArrayList markerPoints = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +46,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng eHillHeritage = new LatLng(-33.802222, 151.286979);
+        //mMap.addMarker(new MarkerOptions().position(eHillHeritage).title("E Hill Heritage Walk"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eHillHeritage, 15));
         mMap.setMinZoomPreference(10.0f);
         mMap.setMaxZoomPreference(20.0f);
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(15.0f));
+
+        // function to pass coordinates to onMapPoints
+        parseCoord();
+
+        // function for onMapPoints
     }
+
+    private void parseCoord () {
+        LatLng eEsplanade = new LatLng(-33.802222, 151.286979);
+        LatLng sSteyne = new LatLng(-33.797286, 151.288127);
+        onMapPoints(eEsplanade, sSteyne);
+    }
+
+    public void onMapPoints (LatLng start, LatLng end) {
+
+        //get initial size of markerPoints array
+        int pointArrSize = markerPoints.size();
+
+        //limiter to how many walks are on the map
+        if (markerPoints.size() > 1) {
+            markerPoints.clear();
+            mMap.clear();
+        }
+
+        //create array for walk (start, end) and add them
+        ArrayList <LatLng> walk = new ArrayList<LatLng>();
+        walk.add(start);
+        walk.add(end);
+
+        //add walk to markerPoints array
+        markerPoints.add(walk);
+
+        //create MarkerOptions for start and end
+        MarkerOptions startOpt = new MarkerOptions();
+        MarkerOptions endOpt = new MarkerOptions();
+
+        startOpt.position(start);
+        endOpt.position(end);
+
+        startOpt.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+        endOpt.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+
+        mMap.addMarker(startOpt);
+        mMap.addMarker(endOpt);
+
+        // check that new points were captured
+        /**
+         * if (markerPoints.size() >= pointArrSize+2) { }
+         */
+    }
+
 }
