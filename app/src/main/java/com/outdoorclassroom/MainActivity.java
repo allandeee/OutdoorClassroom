cef60b18608bd396af8d5cf8340cd7d6d9ccce52
Allan Dominguez <allan.dominguez@students.mq.edu.au>
2017-04-30 12:05:57 +1000

    Implemented Index/Main Menu
    Index to the 3 componentes of the app
    - Manly Map: to map of walks
    - Browser: (to be implemented)
    - Database: (to be implemented)

--- app/src/main/java/com/outdoorclassroom/MainActivity.java
+++ app/src/main/java/com/outdoorclassroom/MainActivity.java
@@ -4,24 +4,33 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button btn = (Button) findViewById(R.id.maps_btn);
-        btn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, MapsActivity.class));
-            }
-        });
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.map:
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+                break;
+            case R.id.browser:
+                Toast.makeText(getBaseContext(), "Browser to be implemented", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.info:
+                Toast.makeText(getBaseContext(), "Database to be implemented", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(getBaseContext(), "Please select one of the options", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
